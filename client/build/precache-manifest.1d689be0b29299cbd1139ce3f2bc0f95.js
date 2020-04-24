b887cbaf8ee8db0c67426d7bdd1c2aafb3ae3528
acarrillo2019 <alric21@aol.com>
2020-04-24 15:23:19 -0700

    npm run build

---
+++ client/build/precache-manifest.1d689be0b29299cbd1139ce3f2bc0f95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "774dd5385bcad01ea6176f9a9b17b0b1",
+    "url": "/static/media/empty.774dd538.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "40c17763e5b597dd9142",
+    "url": "/static/js/main.3d5de96a.chunk.js"
+  },
+  {
+    "revision": "140ecae0b31feb5f1713",
+    "url": "/static/js/2.b8f90034.chunk.js"
+  },
+  {
+    "revision": "40c17763e5b597dd9142",
+    "url": "/static/css/main.b4b060c9.chunk.css"
+  },
+  {
+    "revision": "25b0c54c003fffce921a3e2cf2873f2b",
+    "url": "/index.html"
+  }
+];
