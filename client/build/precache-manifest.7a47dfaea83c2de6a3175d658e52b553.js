2e0cbc4c4ce0ba145269fe2ef509e269f0af5b0e
acarrillo2019 <alric21@aol.com>
2020-04-25 04:19:31 -0700

    top

---
+++ client/build/precache-manifest.7a47dfaea83c2de6a3175d658e52b553.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "774dd5385bcad01ea6176f9a9b17b0b1",
+    "url": "/static/media/empty.774dd538.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8df0e0d43aa0befd4419",
+    "url": "/static/js/main.2c1203e9.chunk.js"
+  },
+  {
+    "revision": "70b5c7e46ef5bb67807b",
+    "url": "/static/js/2.bdce56e3.chunk.js"
+  },
+  {
+    "revision": "8df0e0d43aa0befd4419",
+    "url": "/static/css/main.a47328d1.chunk.css"
+  },
+  {
+    "revision": "65aa07748b99815dc158e6c095221184",
+    "url": "/index.html"
+  }
+];
